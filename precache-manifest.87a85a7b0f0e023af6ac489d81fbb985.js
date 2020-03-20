55e34d1d43c29363b62af0b6a4529470c845201d
gil121983 <gilstolar@icloud.com>
2020-03-20 21:21:43 +0200

    Updates

---
+++ precache-manifest.87a85a7b0f0e023af6ac489d81fbb985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8a3f952c4a94757b9b636a6d36c33a",
+    "url": "/chefbook/index.html"
+  },
+  {
+    "revision": "1b3f18f8b5a62e01d8c3",
+    "url": "/chefbook/static/css/main.81fa9196.chunk.css"
+  },
+  {
+    "revision": "cff1f0791c260fbe2001",
+    "url": "/chefbook/static/js/2.8add4d5b.chunk.js"
+  },
+  {
+    "revision": "fc286fddefd46b8628057a6f1be892bd",
+    "url": "/chefbook/static/js/2.8add4d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3f18f8b5a62e01d8c3",
+    "url": "/chefbook/static/js/main.f5c10c37.chunk.js"
+  },
+  {
+    "revision": "4885d36d8dbc9b666c9a",
+    "url": "/chefbook/static/js/runtime-main.3902e6b5.js"
+  }
+]);
